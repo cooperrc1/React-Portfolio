f90d94a82587e4c096c76789d78a37b4a6e32350
cooperrc <coopercutbirth@yahoo.com>
2021-08-03 20:21:20 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Hey, I'm Cooper
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/cooperrc1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
